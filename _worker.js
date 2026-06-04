93c1df87aee1bf88689933df43aadc4c4dd87b15
Hack-cheng <125268023+Hack-cheng@users.noreply.github.com>
2026-06-04 13:35:45 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -284,7 +284,7 @@ function gh(env, path, opts = {}) {
   return fetch(`https://api.github.com${path}`, {
     ...opts,
     headers: {
-      Authorization: `Bearer ${env.GH_PAT}`,
+      Authorization: `Bearer ${env.GITHUB_TOKEN}`,
       Accept: 'application/vnd.github+json',
       'X-GitHub-Api-Version': '2022-11-28',
       'User-Agent': 'APK-Builder-CF-Worker/1.0',
